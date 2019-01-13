23c66eb83e1bd1139541c1f8526b1de7d5152d4c
lacanlale <gizmo1398@gmail.com>
2019-01-12 17:02:35 -0800

    Finished day3p2

--- solutions/day3p2.py
+++ solutions/day3p2.py
@@ -30,18 +30,26 @@ def label_input(claim="#0 @ 0,0: 0X0"):
     }
     return claim_dict
 
-
+# Map each claim onto the fabric and record their areas
 fabric = np.full((1000,1000), 0)
+claim_areas = {}
 for claim in claim_input:
     elf_claim = label_input(claim=claim)
+    claim_areas[elf_claim['claim_id']] = elf_claim['width'] * elf_claim['length']
     print("-= MAPPING CLAIM {} =-".format(elf_claim['claim_id']))
-
     for row in range(elf_claim['row'], elf_claim['row'] + elf_claim['width']):
         if row >= 1000:
             break
         for col in range(elf_claim['col'], elf_claim['col'] + elf_claim['length']):
             if col >= 1000:
                 break
-            fabric[row][col] = 9999 if fabric[row][col] else elf_claim['claim_id']
+            fabric[row][col] = -1 if fabric[row][col] else elf_claim['claim_id']
 
-print(np.unique(fabric, return_counts='True'))
+items, counts = np.unique(fabric, return_counts=True)
+repetitions = dict(zip(items, counts))
+
+# Check for equivalent areas and repetitions
+for key in claim_areas:
+    if claim_areas[key] == repetitions.get(int(key), 0):
+        print(f"!! ANS: {key} !!")
+        break
